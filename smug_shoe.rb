fc6ff3d2ab3048a30e953e86e6ff40b099ba9310
cajun <zac@kleinpeter.org>
2009-04-10 16:48:02 -0500

    Version 0.1.0 ready?

--- smug_shoe.rb
+++ smug_shoe.rb
@@ -4,21 +4,28 @@ end
 
 require 'smile'
 
-Shoes.app :title => 'Smug Shooe', :width => 1050, :radius => 12 do
-  smug = Smile::Smug.new
-  smug.auth_anonymously
+class SmugShooe < Shoes
+  url '/', :index
   
-  background gradient slategray, gray
-  stroke white
-  para 'Enter NickName ( e.g. kleinpeter )'
-  @nick = edit_line :text => 'vincentlaforet'
-  
-  
-  button 'get albums' do
-    albums_hash = {}
+  def index
+    @smug = Smile::Smug.new
+    @smug.auth_anonymously
     
-    smug.albums( :NickName => @nick.text, :Heavy => 1 ).each do |album|
-      albums_hash[album.title] = album
+    para 'Nick Name ( e.g. kleinpeter )'
+    @nick = edit_line :text => 'vincentlaforet'
+    button 'get albums' do
+      fetch_albums
+    end
+    
+    @albums = flow{}
+    @photos = flow{}
+  end
+  
+  def fetch_albums
+    hash = {}
+    
+    @smug.albums( :NickName => @nick.text, :Heavy => 1 ).each do |album|
+      hash[album.title] = album
     end
     
     @albums.clear
@@ -26,40 +33,44 @@ Shoes.app :title => 'Smug Shooe', :width => 1050, :radius => 12 do
     
     @albums.append do
       para 'Albums'
-      @albums_list = list_box :items => [ "Pick One" ] + albums_hash.keys do |album_text|
-        display_album( albums_hash[album_text.text] )
+      @albums_list = list_box :items => [ "Pick One" ] + hash.keys do |album_text|
+        display_album( hash[album_text.text] )
       end
     end
   end
   
-  @albums = flow{ }
-  @photos = flow{ }
+  def default_background
+    background gradient slategray, gray
+  end
   
+  def box_background
+    background white, :curve => 10
+  end
 
   def display_album( album )
     @photos.clear
     @photos.append do
-      stack do
-        background white, :curve => 10, :margin => 10, :margin_right => 10 + gutter
+      stack :margin => 10, :margin_right => 10 + gutter do
+        box_background
         title album.title, :align => 'center'
-        caption album.description, :align => 'center'
+        para snippet( album.description, 50 ), :align => 'center'
         inscription "Pic Count: #{album.image_count}", :align => 'center'
       end
       
       flow :margin => 10, :margin_right => 10 + gutter do
-        background white, :curve => 10, :center => true
-        display_photos( album)
+        box_background
+        display_photos( album ) if( album.image_count.to_i > 1 )
       end
     end
   end
-  
+
   def display_photos( album )
     flow :margin => 5 do
       album.photos.each do |photo|
         pic = show_image photo.thumb_url, :margin => 3, :center => true
 
         pic.click do |button, left, top|
-          pic_dialog( album, photo )
+          photo_and_vid_details( album, photo )
         end 
       end
     end
@@ -73,26 +84,37 @@ Shoes.app :title => 'Smug Shooe', :width => 1050, :radius => 12 do
     end
   end
   
-  def pic_dialog( album, photo )
-    @details = window :title => album.title do
-      background gradient slategray, gray
-      stack :margin => 5, :margin_right => gutter + 5 do
-        background white, :curve => 10, :center => true
-        para( link( album.title ), :align => 'center' ).click{ system( photo.album_url ) }
+  def snippet( text, length = 20 )
+    text[0, length] + "..." if( text.length > length + 1 )
+  end
+
+  def photo_and_vid_details( album, photo )
+    
+    @photos.clear
+    @photos.append do
+      stack :margin => 10, :margin_right => 10 + gutter do
+        box_background
+        para( link( album.title ) {
+          @vid.stop if( @vid )
+          display_album( album ) 
+        }, :align => 'center' )
         para "Last Updated: #{photo.last_updated}", :align => 'center'
-        para "Caption: #{photo.caption}", :align => 'center'
-      end # dialog
+        para "Caption: #{snippet( photo.caption )}", :align => 'center'
+      end
       
-      flow :margin => 5, :margin_right => gutter + 5 do
-        background white, :curve => 10, :center => true, :margin_right => gutter
+      flow :margin => 10, :margin_right => 10 + gutter do
+        box_background
+        
         %w( thumb small medium large x_large x2_large x3_large original ).each do |p|
           url = photo.send( "#{p}_url" )
           if( url )
             caption link( p, :click => Proc.new do
-                @box.clear
-                @box.append do
-                  background white, :curve => 10, :center => true
-                  image url, :margin => 5, :center => true
+                @controls.hide if( @controls )
+                @status.show if( @status )
+                @details.clear
+                @details.append do
+                  box_background
+                  show_image url, :margin => 5, :center => true
                 end
               end
             )
@@ -103,42 +125,48 @@ Shoes.app :title => 'Smug Shooe', :width => 1050, :radius => 12 do
           url = photo.send( "#{p}_url" )
           if( url )
             caption link( p, :click => Proc.new do
-                @box.clear
-                @box.append do
-                  background white, :curve => 10
+                @controls.show if( @controls )
+                @status.show if( @status )
+                @details.clear
+                @details.append do
+                  box_background
                   stack :margin => 5 do
+                    @vid.stop if( @vid )
                     @vid = video url
                   end
                 
-                  para "controls: ",
-                    link("play")  { @vid.play }, ", ",
-                    link("pause") { @vid.pause }, ", ",
-                    link("stop")  { @vid.stop }, ", ",
-                    link("-5 sec") { @vid.time -= 5000 }, ", ",
-                    link("+5 sec") { @vid.time += 5000 }, ", ",
-                    link("done") { @vid.stop; close }
-                  
-                  @status = para "status: "
-                  #every( 1 ) do |count|
-                  #  if( @vid.length.nil? )
-                  #    @status.text "status: loading#{'.' * ( count % 3 ) }" 
-                  #  else
-                  #    @status.text "status: #{@vid.time / 1000} / #{@vid.length / 1000}"
-                  #  end
-                  #end
+                  every( 1 ) do |count|
+                    unless( @vid.length.nil? )
+                      @status.replace "#{@vid.time / 1000} sec / #{@vid.length / 1000} sec"
+                    end
+                  end
                 end
               end
             )
           end
         end
         
+        @details = stack :margin => 5, :margin_right => gutter + 5 do
+          background white, :curve => 10, :center => true
+          image photo.small_url, :margin => 5, :center => true
+        end
+        
+        @controls = para "controls: ",
+          link("play")  { @vid.play }, ", ",
+          link("pause") { @vid.pause }, ", ",
+          link("stop")  { @vid.stop }, ", ",
+          link("-5 sec") { @vid.time -= 5000 }, ", ",
+          link("+5 sec") { @vid.time += 5000 }, ", "
+        
+        @status = para ""
+        @status.hide
+        @controls.hide
       end
-      
-      @box = stack :margin => 5, :margin_right => gutter + 5 do
-        background white, :curve => 10, :center => true
-        image photo.small_url, :margin => 5, :center => true
-      end
-    end # pic click
+    end
   end
+  
 end
+
+
+Shoes.app :title => 'Smug Shooe', :width => 800, :height => 800, :resizeable => false
  
