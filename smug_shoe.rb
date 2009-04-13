dd7adb9a93243808a896ff3db30abc285c0c3343
cajun <zac@kleinpeter.org>
2009-04-13 08:51:14 -0500

    Split apart app to make it look more like shooooooes

--- smug_shoe.rb
+++ smug_shoe.rb
@@ -1,79 +1,119 @@
+Shoes.setup { source 'http://gems.github.com' }
+
 Shoes.setup do
-  sources = [ 'http://gems.rubyforge.org/', 'http://gems.github.com' ]
   gem 'cajun-smile'
 end
 
 require 'smile'
+require 'cgi'
 
-class SmugShooe < Shoes
-  url '/', :index
-  url '/albums', :albums
-  url '/photos/(\w+)', :photos
-  url '/photo_and_vid_details/(\w+),(\w+)'
+class Session
+  include Singleton
   
-  def index
+  attr_accessor :smug, :albums
+  def init
     @smug = Smile::Smug.new
     @smug.auth_anonymously
+  end
+  
+  def smug
+    init if( @smug.nil? )
+    @smug
+  end
+  
+  def albums=( value )
+    @albums = value
+  end
+end
+
+class Shooes < Shoes
+  attr_accessor :header_height
+  def header
+    @session = Session.instance
     background black
-    
-    para 'Nick Name ( e.g. kleinpeter )', :stroke => white
-    @nick = edit_line :text => 'vincentlaforet'
-    button 'get albums' do
-      fetch_albums
+    @header_height = 90
+    stack :top => 0, :left => 0, :height => @header_height, :attach => Window do
+      title 'Smug Shooes!!!', :align => 'center', :stroke => white
+      flow do
+        inscription em( link( 'Home' ) { default_on_click_home } ), " ",
+          em( link( 'SmugMug', :click => 'http://www.smugmug.com/' ) ),
+          :align => 'center'
+      end
     end
-    
-    @albums = stack :margin => 10, :margin_right => 10 + gutter
-    @album_display = flow do
-      @photo_vid = stack :margin => 5, :margin_right => 200 + gutter, :attach => Window, 
-        :top => 25, :left => 200
-      @photos = stack :width => -600, :margin => 5, :margin_right => 5 + gutter, :top => 20,
-        :attach => self, :top => 20
-    end
+  end
+  
+  def on_home_click
+  end
+  
+  def default_on_click_home
+    on_home_click
+    visit( '/' )
   end
   
   def box_background
     background white, :curve => 10
   end
+
+  def unescapeHTML( text )
+    CGI.unescapeHTML( text )
+  end
   
-  def fetch_albums
-    @vid.stop if( @vid )
-    @photo_vid.clear
-    @albums.clear
-    @photos.clear
+  def show_image( photo_url, options )
+    if( photo_url )
+      image photo_url, options
+    else
+      para "No Image", :stroke => white
+    end
+  end
+end
+
+class Albums < Shooes
+  url '/albums/(\w+)', :index
+  
+  def index( nick )
+    header
     
-    @albums.append do
-      @smug.albums( :NickName => @nick.text, :Heavy => 1 ).each do |album|
-        if( album.image_count.to_i > 1 )
-          flow :margin => 5 do
-            box_background
-            first_photo = album.photos.first.thumb_url
-          
-            if( first_photo )
-              stack :width => 200 do
-                pic = show_image first_photo, :margin => 6
-                pic.click do |button, left, top|
-                  @albums.clear
-                  display_photos( album )
-                end
-              end
-            
-              stack :width => -200 do
-                subtitle album.title
-                para snippet( album.description, 50 )
-                inscription "Pic Count: #{album.image_count}"
-              end
-            end # if first
-          end # if > 1
+    @session.albums = @session.smug.albums( :NickName => nick, :Heavy => 1 ).select{ |x| x.image_count.to_i > 1 }
+    flow :top => @header_height do
+      @session.albums.each_with_index do |album,index|
+        flow :margin => 5, :margin_right => 5 + gutter do
+          box_background
+          stack :width => 200 do
+            pic = show_image album.photos.first.thumb_url, :margin => 6
+            pic.click do |button, left, top|
+              visit( "/images/#{index}" )
+            end
+          end
+
+          stack :width => -200 do
+            para album.title
+            inscription unescapeHTML( album.description )
+            inscription em "Pic Count: #{album.image_count}"
+          end
         end
       end
     end
   end
+end
 
+class Images < Shooes
+  url '/images/(\d+)', :index
+  
+  def setup
+    @album_display = flow do
+      @photo_vid = stack :margin_right => 200 + gutter, 
+        :attach => Window, 
+        :top => 5, :left => 200
+      @photos = stack :width => -600, :margin => 5, :margin_right => 5 + gutter, :top => 20,
+        :attach => self, :top => 20
+    end
+  end
+  
+  def index( album_number )
+    header
+    setup
+    album = @session.albums[album_number.to_i]
 
-  def display_photos( album )
-    @photos.clear
-    @photo_vid.clear
-    
     @photos.append do
       album.photos.each do |photo|
         pic = show_image photo.thumb_url, :margin => 3, :center => true
@@ -85,101 +125,104 @@ class SmugShooe < Shoes
     end
     @photos.scroll = true
     photo_and_vid_details( album, album.photos.first )
-  end
-  
-  def show_image( photo_url, options )
-    if( photo_url )
-      image photo_url, options
-    else
-      para "No Image", :stroke => white
-    end
-  end
-  
-  def snippet( text, length = 20 )
-    text[0, length] + "..." if( text.length > length + 1 )
-  end
+   end
+   
+   def on_home_click
+    @vid.stop if( @vid )
+   end
+   
+   def photo_and_vid_details( album, photo )
+     @photo_vid.clear
+     @photo_vid.append do
+       stack :top => @header_height, :margin => 2, :margin_right => 2 + gutter do
+         box_background
+         inscription( 
+           link( "View #{album.title} at SmugMug", :click => photo.album_url ), 
+           :align => 'center' 
+         )
+         inscription em "Last Updated: #{photo.last_updated}", :align => 'center'
+         inscription unescapeHTML( photo.caption ), :align => 'center'
+       end
 
-  def photo_and_vid_details( album, photo )
+       flow :margin => 2, :margin_right => 2 + gutter do
+         box_background
+
+         %w( thumb small medium large x_large x2_large x3_large original ).each do |p|
+           url = photo.send( "#{p}_url" )
+           if( url )
+             inscription link( p, :click => Proc.new do
+                 @controls.hide if( @controls )
+                 @status.show if( @status )
+                 @details.clear
+                 @details.append do
+                   box_background
+                   show_image url, :margin => 5, :center => true
+                 end
+               end
+             )
+           end
+         end
+
+         %w( video320 video640 video960 video1280 ).each do |p|
+           url = photo.send( "#{p}_url" )
+           if( url )
+             inscription link( p, :click => Proc.new do
+                 @controls.show if( @controls )
+                 @status.show if( @status )
+                 @details.clear
+                 @details.append do
+                   box_background
+                   stack :margin => 5 do
+                     @vid.stop if( @vid )
+                     @vid = video url, :autoplay => true
+                   end
+
+                   every( 1 ) do |count|
+                     unless( @vid.length.nil? )
+                       @status.replace "#{@vid.time / 1000} sec / #{@vid.length / 1000} sec"
+                     end
+                   end
+                 end
+               end
+             )
+           end
+         end
+
+         @details = stack :margin => 5, :margin_right => gutter + 5 do
+           background white, :curve => 10, :center => true
+           image photo.small_url, :margin => 5, :center => true
+         end
+
+         @controls = para "controls: ",
+           link("play")  { @vid.play }, ", ",
+           link("pause") { @vid.pause }, ", ",
+           link("stop")  { @vid.stop }, ", ",
+           link("-5 sec") { @vid.time -= 5000 }, ", ",
+           link("+5 sec") { @vid.time += 5000 }, ", "
+
+         @status = para ""
+         @status.hide
+         @controls.hide
+       end
+     end
+   end
+end
+
+class SmugShooe < Shooes
+  url '/', :index
+  
+  def index
+    header
     
-    @photo_vid.clear
-    @photo_vid.append do
-      stack :margin => 10, :margin_right => 10 + gutter do
-        box_background
-        para( 
-          link( "View #{album.title} at SmugMug", :click => photo.album_url ), 
-          :align => 'center' 
-        )
-        para "Last Updated: #{photo.last_updated}", :align => 'center'
-        para "Caption: #{snippet( photo.caption )}", :align => 'center'
-        para( 
-          link( 'Back to Albums' ) { fetch_albums }, 
-          :align => 'center'
-        )
-      end
-      
-      flow :margin => 10, :margin_right => 10 + gutter do
-        box_background
-        
-        %w( thumb small medium large x_large x2_large x3_large original ).each do |p|
-          url = photo.send( "#{p}_url" )
-          if( url )
-            caption link( p, :click => Proc.new do
-                @controls.hide if( @controls )
-                @status.show if( @status )
-                @details.clear
-                @details.append do
-                  box_background
-                  show_image url, :margin => 5, :center => true
-                end
-              end
-            )
-          end
-        end
-        
-        %w( video320 video640 video960 video1280 ).each do |p|
-          url = photo.send( "#{p}_url" )
-          if( url )
-            caption link( p, :click => Proc.new do
-                @controls.show if( @controls )
-                @status.show if( @status )
-                @details.clear
-                @details.append do
-                  box_background
-                  stack :margin => 5 do
-                    @vid.stop if( @vid )
-                    @vid = video url, :autoplay => true
-                  end
-                
-                  every( 1 ) do |count|
-                    unless( @vid.length.nil? )
-                      @status.replace "#{@vid.time / 1000} sec / #{@vid.length / 1000} sec"
-                    end
-                  end
-                end
-              end
-            )
-          end
-        end
-        
-        @details = stack :margin => 5, :margin_right => gutter + 5 do
-          background white, :curve => 10, :center => true
-          image photo.small_url, :margin => 5, :center => true
-        end
-        
-        @controls = para "controls: ",
-          link("play")  { @vid.play }, ", ",
-          link("pause") { @vid.pause }, ", ",
-          link("stop")  { @vid.stop }, ", ",
-          link("-5 sec") { @vid.time -= 5000 }, ", ",
-          link("+5 sec") { @vid.time += 5000 }, ", "
-        
-        @status = para ""
-        @status.hide
-        @controls.hide
+    flow :top => @header_height do
+      para 'Nick Name ( e.g. kleinpeter )', :stroke => white
+      nick = edit_line :text => 'vincentlaforet'
+    
+      button 'get albums' do
+        visit( "/albums/#{nick.text}" )
       end
     end
   end
-  
 end
 
 
